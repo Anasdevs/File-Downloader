2464383a2f1b863cbebfcd8b600fe7842d4041ca
Anasdevs <anassaifi1108@gmail.com>
2023-08-08 17:36:26 +0530

    Added regex to match url

--- index.js
+++ index.js
@@ -2,18 +2,26 @@ const urlInput = document.querySelector("input");
 const downloadBtn = document.querySelector("button");
 
 downloadBtn.addEventListener("click", async () => {
-    if(urlInput.value===""){
-        alert("Please paste the url!");
-        return
+    if (urlInput.value === "") {
+        alert("Please paste the URL!");
+        return;
     }
-    try{
+
+    const urlPattern = /^(http|https):\/\/[^ "]+$/;
+
+    if (!urlPattern.test(urlInput.value)) {
+        alert("Please enter a valid URL!");
+        return;
+    }
+
+    try {
         const response = await fetch(urlInput.value);
         const file = await response.blob();
         const link = document.createElement("a");
         link.href = URL.createObjectURL(file);
-        link.download = new Date().getTime();
+        link.download = new Date().getTime(); 
         link.click();
-    } catch(error){
-        alert("Failed to download!")
+    } catch (error) {
+        alert("Failed to download!");
     }
-})
+});
